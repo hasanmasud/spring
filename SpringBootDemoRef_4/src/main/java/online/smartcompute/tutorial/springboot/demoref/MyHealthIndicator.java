910c75551fffc012395de40aaf32dc538aa4c169
hasanmasud <masudbook@gmail.com>
2017-03-10 02:26:38 +0100

    commit

---
+++ SpringBootDemoRef_4/src/main/java/online/smartcompute/tutorial/springboot/demoref/MyHealthIndicator.java
@@ -0,0 +1,17 @@
+package online.smartcompute.tutorial.springboot.demoref;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyHealthIndicator implements HealthIndicator {
+	@Override
+	public Health health() {
+		int errorCode = 0; // perform some specific health check
+		if (errorCode != 0) {
+			return Health.down().withDetail("Error Code", errorCode).build();
+		}
+		return Health.up().build();
+	}
+}
